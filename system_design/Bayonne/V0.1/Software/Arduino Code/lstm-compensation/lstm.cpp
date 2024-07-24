653235452a91b150d3b8a42b5f330c9cc08bd3e8
JMcGuireCE <jpm31@email.sc.edu>
2024-07-24 16:41:51 -0400

    Fixed activation functions
    Fixed the issue with the activation functions that was causing the
    exploding initial state problem. This has been tested, compared against
    the same data running through Keras, and shown to be effective.

--- system_design/Bayonne/V0.1/Software/Arduino Code/lstm-compensation/lstm.cpp
+++ system_design/Bayonne/V0.1/Software/Arduino Code/lstm-compensation/lstm.cpp
@@ -7,20 +7,9 @@ using edgeML::LSTM;
 LSTM::LSTM(int numUnits, int inputSize, float** mergedWeightMatrix,
            float* biasVector) : numUnits(numUnits), inputSize(inputSize),
                                 mergedWeightMatrix(mergedWeightMatrix), biasVector(biasVector) {
-  this->statesVector = new float[numUnits + inputSize];
-  this->cellState = new float[numUnits];
-  this->gates = new float[numUnits * 4];
-
-  for (int i = 0; i < inputSize; i++) {
-    this->statesVector[i] = 0;
-  }
-
-  for (int i = 0; i < numUnits; i++) {
-    this->statesVector[inputSize + i] = 0;
-    this->cellState[i] = 0;
-    this->gates[i] = this->gates[numUnits + i] = this->gates[2 * numUnits + i] =
-      this->gates[3 * numUnits + i] = 0;
-  }
+  this->statesVector = new float[numUnits + inputSize]();
+  this->cellState = new float[numUnits]();
+  this->gates = new float[numUnits * 4]();
 }
 
 // Forward pass
@@ -36,9 +25,9 @@ void LSTM::step(float* input, float* destination) {
   // Apply activation functions
   for (int i = 0; i < numUnits; i++) {
     cellState[i] = cellState[i] * sigmoid(gates[numUnits + i]) +
-                   sigmoid(gates[i]) * hypertan(3 * numUnits + i);
+                   sigmoid(gates[i]) * hypertan(gates[2 * numUnits + i]);
 
     destination[i] = statesVector[inputSize + i] =
-      sigmoid(gates[2 * numUnits + i]) * hypertan(cellState[i]);
+      sigmoid(gates[3 * numUnits + i]) * hypertan(cellState[i]);
   }
 }
