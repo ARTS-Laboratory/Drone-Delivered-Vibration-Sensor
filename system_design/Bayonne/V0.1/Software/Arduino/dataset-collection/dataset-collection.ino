7b1911cefa33abdceab4b34f80e7dde01ea62abb
JMcGuireCE <jpm31@email.sc.edu>
2024-12-07 15:59:56 -0500

    Lower data collection sample rate

--- system_design/Bayonne/V0.1/Software/Arduino/dataset-collection/dataset-collection.ino
+++ system_design/Bayonne/V0.1/Software/Arduino/dataset-collection/dataset-collection.ino
@@ -21,7 +21,7 @@ const uint8_t TRIGGER_PIN = 2;
 constexpr uint32_t SPI_SPEED = 2000000;
 constexpr size_t DATA_POINTS = 120000;
 
-constexpr uint32_t FREQUENCY = 1600;  // Sampling rate of the accelerometer (Hz)
+constexpr uint32_t FREQUENCY = 400;  // Sampling rate of the accelerometer (Hz)
 constexpr uint32_t DELAY_TIME =
   static_cast<uint32_t>(((1.0 / FREQUENCY) * 1000000));  // Period (us)
 
