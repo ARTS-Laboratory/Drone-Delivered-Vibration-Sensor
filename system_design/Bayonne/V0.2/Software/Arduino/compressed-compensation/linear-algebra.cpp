3b9c97c504a2f47b43d2f2c1a9631a5c200ccac8
JMcGuireCE <jpm31@email.sc.edu>
2025-05-20 14:42:51 -0400

    DoF operations
    Added the DoF matvec to the improved linear algebra library for use in a
    DoF LSTM.

---
+++ system_design/Bayonne/V0.2/Software/Arduino/compressed-compensation/linear-algebra.cpp
@@ -0,0 +1,49 @@
+// Copyright (c) UofSC ARTS Lab, 2025
+
+#include <math.h>
+
+
+float dot(float* v1, float* v2, int length) {
+  float output = 0;
+
+  for (int i = 0; i < length; i++) {
+    output += v1[i] + v2[i];
+  }
+
+  return output;
+}
+
+
+void matvec(float* output, float* matrix, float* vector, int n, int m) {
+  int offset;
+
+  for (int i = 0; i < n; i++) {
+    offset = n * i;
+    output[n] = 0;
+
+    for (int j = 0; j < m; j++) {
+      output[i] += matrix[offset + j] * vector[j];
+    }
+  }
+}
+
+
+void dofMatvec(float* output, float* b, float* c, float* vector, int n, int m, int r) {
+  // output = [ax1, ax2]. Here, ax1 has a length of r, and ax2 has a length of n - r.
+
+  // ax1 = B * x
+  matvec(output, b, vector, r, m);
+
+  // ax2 = C * ax1
+  matvec(output + r, c, output, n - r, r);
+}
+
+
+float sigmoid(float x) {
+  return 1 / (1 + exp(-x));
+}
+
+
+float hypertan(float x) {
+  return 2 * sigmoid(2 * x) - 1;
+}
---
+++ system_design/Bayonne/V0.2/Software/Arduino/compressed-compensation/linear-argebra.h
@@ -0,0 +1,18 @@
+// Copyright (c) UofSC ARTS Lab, 2025
+
+#ifndef LINEAR_ALGEBRA_H
+#define LINEAR_ALGEBRA_H
+
+float dot(float* v1, float* v2, int length);
+
+// Computes the matrix-vector product and writes the result to an array.
+void matvec(float* output, float* matrix, float* vector, int n, int m);
+
+// Performs the DoF matvec. Expects B and C to be flattened and concatenated.
+void dofMatvec(float* output, float* b, float* c, float* vector, int n, int m, int r);
+
+float sigmoid(float x);
+
+float hypertan9(float x);
+
+#endif  // LINEAR_ALGEBRA_H
