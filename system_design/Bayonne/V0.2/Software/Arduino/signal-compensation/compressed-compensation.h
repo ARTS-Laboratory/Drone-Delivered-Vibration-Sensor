05b1f2c08b638895465181bbd91f768a02b3871b
JMcGuireCE <jpm31@email.sc.edu>
2025-05-28 14:21:35 -0400

    Load weights from model binary files
    A much smarter way of doing things. This allows us to update the model
    without rewriting the firmware, and converting the model into a C header
    with python and string parsing is "jank".

---
+++ system_design/Bayonne/V0.2/Software/Arduino/signal-compensation/compressed-compensation.h
@@ -0,0 +1,22 @@
+// Copyright ARTS Lab, 2024
+
+#ifndef LSTM_COMPENSATION_H
+#define LSTM_COMPENSATION_H
+
+#include <SCA3300.h>
+#include "lstm.h"
+
+using sca3300_library::OperationMode;
+
+void recordData(float* data, uint32_t delayTime);
+
+void writeSDConverted(float* data, char* fileName);
+
+void printDataConverted(int16_t* data, OperationMode operationMode);
+
+void loadWeights(float* lstmWeights, float* lstmBias, float* denseWeights,
+                 float* denseBias);
+
+float runInference(float* input);
+
+#endif  // LSTM_COMPENSATION_H
