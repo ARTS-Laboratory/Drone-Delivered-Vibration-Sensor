7b79571b2bc8f1edb7c7596f05e36498854d6b70
JMcGuireCE <jpm31@email.sc.edu>
2025-05-29 14:09:15 -0400

    Fix typo

--- system_design/Bayonne/V0.2/Software/Arduino/signal-compensation/linear-algebra.h
+++ system_design/Bayonne/V0.2/Software/Arduino/signal-compensation/linear-algebra.h
@@ -3,6 +3,9 @@
 #ifndef LINEAR_ALGEBRA_H
 #define LINEAR_ALGEBRA_H
 
+#define NUMUNITS 50
+#define INPUTSIZE 1
+
 float dot(float* v1, float* v2, int length);
 
 // Computes the matrix-vector product and writes the result to an array.
