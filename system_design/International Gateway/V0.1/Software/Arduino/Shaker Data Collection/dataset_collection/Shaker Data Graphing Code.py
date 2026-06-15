477adb5459fa6cacae4d8321a8cc10a699f8c76f
aigiese <aigiese@email.sc.edu>
2026-06-15 11:18:12 -0400

    Plotting filtered vs original

--- system_design/International Gateway/V0.1/Software/Arduino/Shaker Data Collection/dataset_collection/Shaker Data Graphing Code.py
+++ system_design/International Gateway/V0.1/Software/Arduino/Shaker Data Collection/dataset_collection/Shaker Data Graphing Code.py
@@ -10,7 +10,7 @@ plt.close('all')
 
 
 #%% Load data
-file1 = 'D:/006.csv'
+file1 = 'E:/006.csv'
 
 def highpass_filter(signal, cutoff, fs, order=4):
     # note to self: signal as in acceleration values (ac), 
@@ -41,80 +41,87 @@ def process_data(filepath):
     ac_filtered = highpass_filter(ac, cutoff=10, fs=fs)
 
     # FFT computation
-    N = len(ac_filtered)                         # number of samples
-    Y = np.fft.fft(ac_filtered)         # FFT
-    f = np.fft.fftfreq(N, d=dt)         # frequency vector
+    N = len(ac)                                    # number of samples
+    Y_original = np.fft.fft(ac)
+    Y_filtered = np.fft.fft(ac_filtered)           # FFT
+    f = np.fft.fftfreq(N, d=dt)                    # frequency vector
 
     # Take only positive frequencies
     mask = f >= 0
     f = f[mask]
-    Y = Y[mask]
+    Y_original = Y_original[mask]
+    Y_filtered = Y_filtered[mask]
     # Amplitude (normalize)
-    A = np.abs(Y) / N
+    A_original = np.abs(Y_original) / N
+    A_filtered = np.abs(Y_filtered) / N
 
     # Convert to dB scale
-    A_dB = 20 * np.log10(A + 1e-12)
-    return tt, ac, ac_filtered, f, A, A_dB
+    A_original_dB = 20 * np.log10(A_original + 1e-12)
+    A_filtered_dB = 20 * np.log10(A_filtered + 1e-12)
+    return (tt, ac, ac_filtered, f, A_original, A_filtered,
+            A_original_dB, A_filtered_dB)
 
 
 #%% Process both files
-tt1, ac1, ac1_filtered, f1, A1, A1_dB = process_data(file1)
+(tt1, ac1, ac1_filtered, f1, 
+A1_original, A1_filtered,
+A1_original_dB, A1_filtered_dB) = process_data(file1)
 
 # size of plot figure (think of it like the measuremetns of a sheet of paper)
 plt.figure(figsize=(6.5,3))
 
 # FFT vs Spectrogram: FFT -> what frequencies exist overall, Spectrogram -> how frequency changes over time
 plt.figure(1)
-plt.plot(f1, A1_dB)
-plt.grid(True)
+plt.plot(f1, A1_original_dB, label='Original')
+plt.plot(f1, A1_filtered_dB, label='Filtered')
 plt.ylabel('Amplitude (dB)')
 plt.xlabel('Frequency (Hz)')
+plt.legend()
 plt.title('006 FFT Spectrum Comparison')
+plt.grid(True)
 # Zoom near region of interest
-plt.xlim(0,200) # up to Nyquist (fs/2 = 200)
+plt.xlim(0,30) # up to Nyquist (fs/2 = 200)
 plt.ylim(-170,-30)
 #Find and mark peak
-peak_idx1 = np.argmax(A1)
-plt.plot(f1[peak_idx1], A1_dB[peak_idx1], 'ro')
-plt.text(f1[peak_idx1] + 2, A1_dB[peak_idx1], f'{f1[peak_idx1]:.1f} Hz', color = 'red')
+# peak_idx1 = np.argmax(A1_filtered)
+# plt.plot(f1[peak_idx1], A1_filtered_dB[peak_idx1], 'ro')
+# plt.text(f1[peak_idx1] + 2, A1_filtered_dB[peak_idx1], f'{f1[peak_idx1]:.1f} Hz', color = 'red')
 plt.tight_layout()
 plt.savefig('C:/Users/giese/OneDrive/Documents/GitHub/Drone-Delivered-Vibration-Sensor/system_design/International Gateway/V0.1/Software/Arduino/Shaker Data Collection/006_FFT_Spectrum_Comparison.png')
 
 window_size = 10
-A1_smooth = np.convolve(A1_dB, np.ones(window_size)/window_size, mode='same')
+A1_original_smooth = np.convolve(A1_original_dB, np.ones(window_size)/window_size, mode='same')
+A1_filtered_smooth = np.convolve(A1_filtered_dB, np.ones(window_size)/window_size, mode='same')
 
 plt.figure(5)
-plt.plot(f1, A1_smooth)
+plt.plot(f1, A1_original_smooth, label='Original')
+plt.plot(f1, A1_filtered_smooth, label='Filtered')
+plt.legend()
 plt.grid(True)
 plt.ylabel('Amplitude (dB)')
 plt.xlabel('Frequency (Hz)')
 plt.title('006 FFT Smoothed Spectrum Comparison')
 # Zoom near region of interest
-plt.xlim(0,200) # up to Nyquist (fs/2 = 200)
+plt.xlim(0,30) # up to Nyquist (fs/2 = 200)
 plt.ylim(-170,-30)
 #Find and mark peak
-peak_idx1 = np.argmax(A1_smooth)
-plt.plot(f1[peak_idx1], A1_smooth[peak_idx1], 'ro')
-plt.text(f1[peak_idx1] + 2, A1_smooth[peak_idx1], f'{f1[peak_idx1]:.1f} Hz', color = 'red')
+# peak_idx1 = np.argmax(A1_filtered_smooth)
+# plt.plot(f1[peak_idx1], A1_filtered_smooth[peak_idx1], 'ro')
+# plt.text(f1[peak_idx1] + 2, A1_filtered_smooth[peak_idx1], f'{f1[peak_idx1]:.1f} Hz', color = 'red')
 plt.tight_layout()
 plt.savefig('C:/Users/giese/OneDrive/Documents/GitHub/Drone-Delivered-Vibration-Sensor/system_design/International Gateway/V0.1/Software/Arduino/Shaker Data Collection/006_FFT_Smoothed_Spectrum_Comparison.png')
 
 plt.figure(2)
-plt.plot(tt1, ac1)
-plt.xlabel('Time (s)')
-plt.ylabel('Acceleration (g)')
-plt.xlim(100,100.5)
-plt.title('006 Zoomed Acceleration Comparison')
-plt.tight_layout() # makes sure plot is neat and sizings work together
-plt.savefig('C:/Users/giese/OneDrive/Documents/GitHub/Drone-Delivered-Vibration-Sensor/system_design/International Gateway/V0.1/Software/Arduino/Shaker Data Collection/006_100_Acceleration_Comparison.png', dpi=300)
-
-plt.figure(6)
 plt.plot(tt1, ac1, label='Original')
-plt.plot(tt1, ac1_filtered, label='High Pass Filtered')
+plt.plot(tt1, ac1_filtered, label='Filtered')
 plt.xlabel('Time (s)')
 plt.ylabel('Acceleration (g)')
+plt.xlim(0,170)
+plt.title('006 Acceleration Comparison')
 plt.legend()
 plt.grid(True)
+plt.tight_layout() # makes sure plot is neat and sizings work together
+plt.savefig('C:/Users/giese/OneDrive/Documents/GitHub/Drone-Delivered-Vibration-Sensor/system_design/International Gateway/V0.1/Software/Arduino/Shaker Data Collection/006_Acceleration_Comparison.png', dpi=300)
 
 
 # # Spectrogram
