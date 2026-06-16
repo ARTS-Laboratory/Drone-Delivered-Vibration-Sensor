80612f8bf6ff48b8038bd6670a392e76b2cebb0c
aigiese <aigiese@email.sc.edu>
2026-06-16 12:27:23 -0400

    2nd LSTM test run results

--- system_design/International Gateway/V0.1/Software/Arduino/Shaker Data Collection/dataset_collection/Shaker Data Graphing Code.py
+++ system_design/International Gateway/V0.1/Software/Arduino/Shaker Data Collection/dataset_collection/Shaker Data Graphing Code.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 from scipy import signal
 from scipy.signal import butter, filtfilt
 from sklearn.preprocessing import StandardScaler
-from tensorflow import keras
+from tensorflow import keras # the package you are using for the LSTM is called tensorflow
 
 
 plt.close('all')
@@ -47,45 +47,46 @@ def process_data(filepath):
 
     # FFT computation
     N = len(ac)                                    # number of samples
-    Y_original = np.fft.fft(ac)
+    Y_ref = np.fft.fft(ac)
     Y_filtered = np.fft.fft(ac_filtered)           # FFT
     f = np.fft.fftfreq(N, d=dt)                    # frequency vector
 
     # Take only positive frequencies
     mask = f >= 0
     f = f[mask]
-    Y_original = Y_original[mask]
+    Y_ref = Y_ref[mask]
     Y_filtered = Y_filtered[mask]
+
     # Amplitude (normalize)
-    A_original = np.abs(Y_original) / N
+    A_ref = np.abs(Y_ref) / N
     A_filtered = np.abs(Y_filtered) / N
 
     # Convert to dB scale
-    A_original_dB = 20 * np.log10(A_original + 1e-12)
+    A_ref_dB = 20 * np.log10(A_ref + 1e-12)
     A_filtered_dB = 20 * np.log10(A_filtered + 1e-12)
-    return (tt, ac, ac_filtered, f, A_original, A_filtered,
-            A_original_dB, A_filtered_dB)
+    return (tt, ac, ac_filtered, f, A_ref, A_filtered,
+            A_ref_dB, A_filtered_dB)
 
 
 #%% Process both files
 (tt1, ac1, ac1_filtered, f1, 
-A1_original, A1_filtered,
-A1_original_dB, A1_filtered_dB) = process_data(file1)
+A1_ref, A1_filtered,
+A1_ref_dB, A1_filtered_dB) = process_data(file1)
 
 
-original = ac1
+ref = ac1
 filtered = ac1_filtered
 input_scaler = StandardScaler()
 target_scaler = StandardScaler()
 filtered_scaled = input_scaler.fit_transform(filtered.reshape(-1,1))
-original_scaled = target_scaler.fit_transform(original.reshape(-1,1))
+ref_scaled = target_scaler.fit_transform(ref.reshape(-1,1))
 
 lstm_window_size = 400 # how much history the network sees
 X = []
 y = []
 for i in range(lstm_window_size, len(filtered_scaled)):
     X.append(filtered_scaled[i-lstm_window_size:i])
-    y.append(original_scaled[i])
+    y.append(ref_scaled[i])
 
 X = np.array(X)
 y = np.array(y)
@@ -120,13 +121,26 @@ model = keras.models.Sequential()
 # 50 units, 1 layer
 model.add(keras.layers.LSTM(50, input_shape=(X_train.shape[1], 1)))
 model.add(keras.layers.Dense(1))
-model.compile(optimizer = 'adam', loss = 'MSE') # tells it how to measure success so it can adjust it's answers
+model.compile(optimizer = 'adam', loss = 'MSE', metrics=[keras.metrics.RootMeanSquaredError()]) # tells it how to measure success so it can adjust it's answers
 #Train
-history = model.fit(X_train, y_train, epochs=20, batch_size=32)
-predictions = model.predict(X_test)
+history = model.fit(X_train, y_train, epochs=5, batch_size=32)
+predictions_all = model.predict(X)
 # turns the units back to what we want
-predictions = target_scaler.inverse_transform(predictions)
+predictions_all = target_scaler.inverse_transform(predictions_all)
 y_test_actual = target_scaler.inverse_transform(y_test.reshape(-1,1))
+prediction_time_all = tt1[lstm_window_size:]
+pred_fft = np.fft.fft(predictions.flatten())
+
+#LSTM Predictions
+N_pred = len(predictions)
+f_pred = np.fft.fftfreq(N_pred, d=np.mean(np.diff(prediction_time)))
+mask_pred = f_pred >= 0
+f_pred = f_pred[mask_pred]
+pred_fft = pred_fft[mask_pred]
+A_pred = np.abs(pred_fft) / N_pred
+A_pred_dB = 20 * np.log10(A_pred + 1e-12)
+
+
 
 
 # size of plot figure (think of it like the measuremetns of a sheet of paper)
@@ -134,8 +148,9 @@ plt.figure(figsize=(6.5,3))
 
 # FFT vs Spectrogram: FFT -> what frequencies exist overall, Spectrogram -> how frequency changes over time
 plt.figure(1)
-plt.plot(f1, A1_original_dB, label='Original')
+plt.plot(f1, A1_ref_dB, label='Reference')
 plt.plot(f1, A1_filtered_dB, label='Filtered')
+plt.plot(f_pred, A_pred_dB, label='LSTM Prediction')
 plt.ylabel('Amplitude (dB)')
 plt.xlabel('Frequency (Hz)')
 plt.legend()
@@ -143,21 +158,24 @@ plt.title('006 FFT Spectrum Comparison')
 plt.grid(True)
 # Zoom near region of interest
 plt.xlim(0,30) # up to Nyquist (fs/2 = 200)
-plt.ylim(-170,-30)
+plt.ylim(-170,-10)
 #Find and mark peak
 # peak_idx1 = np.argmax(A1_filtered)
 # plt.plot(f1[peak_idx1], A1_filtered_dB[peak_idx1], 'ro')
 # plt.text(f1[peak_idx1] + 2, A1_filtered_dB[peak_idx1], f'{f1[peak_idx1]:.1f} Hz', color = 'red')
 plt.tight_layout()
 plt.savefig('C:/Users/giese/OneDrive/Documents/GitHub/Drone-Delivered-Vibration-Sensor/system_design/International Gateway/V0.1/Software/Arduino/Shaker Data Collection/006_FFT_Spectrum_Comparison.png')
+plt.show()
 
 fft_smoothing_window = 10
-A1_original_smooth = np.convolve(A1_original_dB, np.ones(fft_smoothing_window)/fft_smoothing_window, mode='same')
+A1_ref_smooth = np.convolve(A1_ref_dB, np.ones(fft_smoothing_window)/fft_smoothing_window, mode='same')
 A1_filtered_smooth = np.convolve(A1_filtered_dB, np.ones(fft_smoothing_window)/fft_smoothing_window, mode='same')
+A_pred_smooth = np.convolve(A_pred_dB, np.ones(fft_smoothing_window) / fft_smoothing_window, mode='same')
 
 plt.figure(5)
-plt.plot(f1, A1_original_smooth, label='Original')
+plt.plot(f1, A1_ref_smooth, label='Reference')
 plt.plot(f1, A1_filtered_smooth, label='Filtered')
+plt.plot(f_pred, A_pred_smooth, label='LSTM Prediction')
 plt.legend()
 plt.grid(True)
 plt.ylabel('Amplitude (dB)')
@@ -165,42 +183,54 @@ plt.xlabel('Frequency (Hz)')
 plt.title('006 FFT Smoothed Spectrum Comparison')
 # Zoom near region of interest
 plt.xlim(0,30) # up to Nyquist (fs/2 = 200)
-plt.ylim(-170,-30)
+plt.ylim(-170,-10)
 #Find and mark peak
 # peak_idx1 = np.argmax(A1_filtered_smooth)
 # plt.plot(f1[peak_idx1], A1_filtered_smooth[peak_idx1], 'ro')
 # plt.text(f1[peak_idx1] + 2, A1_filtered_smooth[peak_idx1], f'{f1[peak_idx1]:.1f} Hz', color = 'red')
 plt.tight_layout()
-plt.show()
 plt.savefig('C:/Users/giese/OneDrive/Documents/GitHub/Drone-Delivered-Vibration-Sensor/system_design/International Gateway/V0.1/Software/Arduino/Shaker Data Collection/006_FFT_Smoothed_Spectrum_Comparison.png')
+plt.show()
 
-plt.figure(2)
-plt.plot(tt1, ac1, label='Original')
-plt.plot(tt1, ac1_filtered, label='Filtered')
-plt.xlabel('Time (s)')
-plt.ylabel('Acceleration (g)')
-plt.xlim(0,170)
-plt.title('006 Acceleration Comparison')
-plt.legend()
-plt.grid(True)
-plt.tight_layout() # makes sure plot is neat and sizings work together
-plt.savefig('C:/Users/giese/OneDrive/Documents/GitHub/Drone-Delivered-Vibration-Sensor/system_design/International Gateway/V0.1/Software/Arduino/Shaker Data Collection/006_Acceleration_Comparison.png', dpi=300)
+# plt.figure(2)
+# plt.plot(tt1, ac1, label='Reference')
+# plt.plot(tt1, ac1_filtered, label='Filtered')
+# plt.xlabel('Time (s)')
+# plt.ylabel('Acceleration (g)')
+# plt.xlim(0,170)
+# plt.title('006 Acceleration Comparison')
+# plt.legend()
+# plt.grid(True)
+# plt.tight_layout() # makes sure plot is neat and sizings work together
+# plt.savefig('C:/Users/giese/OneDrive/Documents/GitHub/Drone-Delivered-Vibration-Sensor/system_design/International Gateway/V0.1/Software/Arduino/Shaker Data Collection/006_Acceleration_Comparison.png', dpi=300)
 
 plt.figure(6)
-plt.plot(y_test_actual, label = 'Original')
-plt.plot(predictions, label = 'Predicted')
+plt.plot(tt1, ac1, label='Reference', alpha=0.7) # alpha controls transparency of lines
+plt.plot(tt1, ac1_filtered, label='Filtered', alpha=0.7)
+plt.plot(prediction_time_all, predictions_all, label = 'LSTM Prediction')
+plt.xlabel('Time (s)')
+plt.ylabel('Acceleration (g)')
+plt.title('006 Acceleration Comparison')
 plt.legend()
-plt.xlabel('Sample')
-plt.ylabel('Acceleration')
-plt.title('Original vs LSTM Prediction')
 plt.tight_layout()
+plt.savefig('C:/Users/giese/OneDrive/Documents/GitHub/Drone-Delivered-Vibration-Sensor/system_design/International Gateway/V0.1/Software/Arduino/Shaker Data Collection/006_Acceleration_Comparison.png', dpi=300)
 plt.show()
-plt.savefig('C:/Users/giese/OneDrive/Documents/GitHub/Drone-Delivered-Vibration-Sensor/system_design/International Gateway/V0.1/Software/Arduino/Shaker Data Collection/Original_vs_LSTM_Prediction.png', dpi=300)
 
-plt.figure(7)
-plt.plot(X[1000])
-plt.title("Example Filtered Window")
-plt.xlabel("Sample")
-plt.ylabel("Scaled Filtered Acceleration")
+# plt.figure(7)
+# plt.plot(X[1000])
+# plt.title("Example Filtered Window")
+# plt.xlabel("Sample")
+# plt.ylabel("Scaled Filtered Acceleration")
+# plt.tight_layout()
+# plt.show()
+
+plt.figure(8)
+plt.plot(history.history['loss'], label='MSE')
+plt.plot(history.history['root_mean_squared_error'], label='RMSE')
+plt.xlabel('Epoch')
+plt.ylabel('Error')
+plt.title('Training RMSE vs Epoch')
+plt.legend()
+plt.grid(True)
 plt.tight_layout()
 plt.show()
